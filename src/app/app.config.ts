8cdb1d41432ff2d678c3ae71182c9f6a768b5fd0
kathiravan <kathiravans@boodskap.io>
2026-06-06 17:57:05 +0530

    with config code

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,21 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+import { APP_INITIALIZER, ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { ConfigService } from './core/services/config.service';
+
+function initializeApp(configService: ConfigService) {
+  return () => configService.loadConfig();
+}
 
 export const appConfig: ApplicationConfig = {
   providers: [
+     {
+      provide: APP_INITIALIZER,
+      multi: true,
+      useFactory: initializeApp,
+      deps: [ConfigService]
+    },
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
     provideRouter(routes)
---
+++ src/app/core/services/config.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+
+interface AppConfig {
+  prod: boolean;
+  appName: string;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ConfigService {
+
+  private config!: AppConfig;
+
+  async loadConfig(): Promise<void> {
+    const response = await fetch('/config/config.json');
+
+    this.config = await response.json();
+  }
+
+  getConfig(): AppConfig {
+    return this.config;
+  }
+}
